7a255183d029e942e26378a2eedba9aae6149841
linebagner <44021591+linebagner@users.noreply.github.com>
2019-05-27 12:52:23 +0200

    Add files via upload

---
+++ nabojs.js
@@ -0,0 +1,11 @@
+
+// menu (https://www.w3schools.com/howto/howto_js_sidenav.asp)
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// video ()
